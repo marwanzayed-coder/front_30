7ddcd66f57d7652616f6a0e5f92f40228c21e16a
marwanzayed-coder <marwanzayedshaban@gmail.com>
2023-03-01 14:04:16 +0200

    Upload First Challenge

--- 1-contact-page/js/main.js
+++ 1-contact-page/js/main.js
@@ -2,17 +2,18 @@ let form = document.getElementById("form");
 form.addEventListener("submit", sendData);
 function sendData(e) {
   e.preventDefault();
+  let regExEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
   if (e.target[0].value.length == 0) {
     popup("Oops Can't Send Data", "You must write your name");
-  }
-  if (e.target[1].value.length == 0) {
+  } else if (e.target[1].value.length == 0) {
     popup("Oops Can't Send Data", "You must write your email");
-  }
-  let regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (e.target[1].value.length > 0 && !regEx.test(e.target[1].value)) {
+  } else if (
+    e.target[1].value.length > 0 &&
+    !regExEmail.test(e.target[1].value)
+  ) {
     popup("Oops Can't Send Data", "You Have Entered An Invalid Email Address!");
-  }
-  if (e.target[2].value.length > 0) {
+  } else if (e.target[2].value.length > 0) {
     let regExPhone = /^01[0125][0-9]{8}$/gm;
     if (!regExPhone.test(e.target[2].value)) {
       popup(
@@ -20,9 +21,13 @@ function sendData(e) {
         "You Have Entered An Invalid Phone Number!"
       );
     }
-  }
-  if (e.target[3].value.length == 0) {
+  } else if (e.target[3].value.length == 0) {
     popup("Oops Can't Send Data", "You must write your Message");
+  } else {
+    let lenght = e.target.length - 1;
+    for (let i = 0; i <= lenght; i++) {
+      e.target[i].value = "";
+    }
   }
 }
 let inputs = document.querySelectorAll(".form input");
